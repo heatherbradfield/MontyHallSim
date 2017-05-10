38fe54745353e57c5af964ebd210fab1a1921a19
Bradfield <heather.bradfield823@sie-1119-01.csuci.edu>
2017-05-10 16:23:26 -0700

    Changed light blue in MenuPanel

--- src/MenuPanel.java
+++ src/MenuPanel.java
@@ -17,25 +17,20 @@ public class MenuPanel extends JPanel {
      * @param newSim The game applet.
      */
     public MenuPanel(final Sim newSim){
-        Color brown = new Color(213,196,174);
-        Color lightBrown = new Color(244,236,226);
-        Color grey = new Color(227,230,228);
-        Color lightBlue = new Color(190,226,228);
-        Color oceanBlue = new Color(111,211,223);
 
         this.setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
 
         gbc.gridwidth = GridBagConstraints.REMAINDER;
         gbc.fill = GridBagConstraints.HORIZONTAL;
-        setBackground(lightBlue);
+        setBackground(newSim.lightBlue);
 
         JLabel stewie = new JLabel(new ImageIcon(getClass().getResource("Images/MontyHall.png")), SwingConstants.CENTER);
         add(stewie, gbc);
 
         JLabel name = new JLabel("<html>Monty Hall Simulation<br></html>",SwingConstants.CENTER);
         name.setFont(new Font("Impact", Font.BOLD, 80));
-        name.setForeground(oceanBlue);
+        name.setForeground(newSim.oceanBlue);
         add(name);
 
         JButton game = new JButton("Play");
